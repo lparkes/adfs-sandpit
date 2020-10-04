94440af78b92d36143061bf282e35d3094a55d78
Lloyd Parkes <Lloyd.Parkes014@msd.govt.nz>
2020-10-04 23:02:28 +0000

    Remove the inevitable sensitive information from variables.tf and add it to git.

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "dns_zone_id" {
+  type = string
+}
+
+variable "dns_role_arn" {
+  type = string
+}
+
+variable "dns_zone" {
+  type = string
+}
+
+variable "acme_account_email" {
+  type = string
+}
+
+variable "windows_admin_password" {
+  type = string
+}
+
+variable "vault_access_principal" {
+  type = string
+}
+
